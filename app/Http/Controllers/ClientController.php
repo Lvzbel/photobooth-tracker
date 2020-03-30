a4338addb059099131319437fd8e1287674e7f82
Lvzbel <rcoto@protonmail.com>
2020-03-29 22:20:59 -0700

    Feat: Client construc, create and store methods created

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Client;
 
 class ClientController extends Controller
 {
-    //
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
+    
+    public function create()
+    {
+        return view('client.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => ['required', 'string']
+        ]);
+
+        Client::create($validatedData);
+
+        return redirect(route('welcome'));
+    }
 }
