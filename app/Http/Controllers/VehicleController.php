0030b1c25c32204b0012100c08e6b4f3e8fc6699
Lvzbel <rcoto@protonmail.com>
2020-03-29 19:05:22 -0700

    Feat: Vehicle@store method created

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Vehicle;
 
 class VehicleController extends Controller
 {
     public function create()
     {
         return view('vehicles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = request()->validate([
+            'work_order' => ['required', 'digits:7']
+        ]);
+
+        Vehicle::create($validatedData);
+
+        return redirect(route('welcome'));
     }
 }
