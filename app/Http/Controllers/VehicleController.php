686fe9c3ac59112c2a1bf4801fb969cb9e557b8d
Lvzbel <rcoto@protonmail.com>
2020-03-29 20:55:53 -0700

    Feat: Vehicle@create nows saves auth user as an owner of the vehicle

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Vehicle;
+use Illuminate\Support\Facades\Auth;
 
 class VehicleController extends Controller
 {
@@ -18,7 +19,11 @@ class VehicleController extends Controller
             'work_order' => ['required', 'digits:7']
         ]);
 
-        Vehicle::create($validatedData);
+        $userId = ['user_id' => Auth::id()];
+
+        $jointData = array_merge($validatedData, $userId);
+
+        Vehicle::create($jointData);
 
         return redirect(route('welcome'));
     }
