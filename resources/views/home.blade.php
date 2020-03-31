7a8cdb5a91213c21adf890c23bc331c2951be046
Lvzbel <rcoto@protonmail.com>
2020-03-31 01:31:14 -0700

    Refact: Home@index has become root "/"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,14 @@
 
 @section('content')
 <div class="container">
+    <h1>Todays Orders</h1>
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+            <ul class="list-group">
+                @foreach ($work_orders as $work_order)
+                    <li class="list-group-item">{{ $work_order->work_order }}</li>
+                @endforeach
+              </ul>
         </div>
     </div>
 </div>
