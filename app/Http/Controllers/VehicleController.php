e41e018a9ee614f2b6e3ee74699311b9af207223
Lvzbel <rcoto@protonmail.com>
2020-03-30 21:08:32 -0700

    Feat: Vehicle@store now accepts client_id

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -4,25 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Vehicle;
+use App\Client;
 use Illuminate\Support\Facades\Auth;
 
 class VehicleController extends Controller
 {
     public function create()
     {
-        return view('vehicles.create');
+        // Get list of current clients for form select input
+        $clients = Client::all();
+        
+        return view('vehicles.create', compact('clients'));
     }
 
     public function store(Request $request)
     {
         $validatedData = request()->validate([
-            'work_order' => ['required', 'digits:7']
+            'work_order' => ['required', 'digits:7'],
+            'client_id' => ['required', 'digits:1']
         ]);
 
         $userId = ['user_id' => Auth::id()];
 
         $jointData = array_merge($validatedData, $userId);
-
+        
         Vehicle::create($jointData);
 
         return redirect(route('welcome'));
