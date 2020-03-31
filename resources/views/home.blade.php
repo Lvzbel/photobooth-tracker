beb052c8f9594b47d0acf0ca057a94789cc24e7f
Lvzbel <rcoto@protonmail.com>
2020-03-31 12:26:32 -0700

    HTML: decrease size of todays orders

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h1>Todays Orders</h1>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-5">
             <ul class="list-group">
                 @foreach ($work_orders as $work_order)
                     <li class="list-group-item">{{ $work_order->work_order }}</li>
