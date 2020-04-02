600e7fbe9369947eefc8f0245d8d5b93dc464bc1
Lvzbel <rcoto@protonmail.com>
2020-04-02 00:56:09 -0700

    Fix: mispelled container class in "/"

--- resources/views/vehicles/create.blade.php
+++ resources/views/vehicles/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="containter">
+  <div class="container">
     <h1>Add a new vehicle</h1>
     
     <div class="row justify-content-center">
