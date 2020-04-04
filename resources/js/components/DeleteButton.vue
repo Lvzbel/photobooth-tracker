850a604127b0b6804e2c3ec238f6f8a95376dbb5
Lvzbel <rcoto@protonmail.com>
2020-04-03 23:40:49 -0700

    Feat: Vue DeleteButton created but not finish

---
+++ resources/js/components/DeleteButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="btn btn-danger btn-sm">Delete</button>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Delete Button Mounted");
+  }
+};
+</script>
+
+<style>
+</style>
