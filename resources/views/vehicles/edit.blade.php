d37bd1cc103eb30502d102485f15381b486cda8f
Lvzbel <rcoto@protonmail.com>
2020-04-04 20:49:15 -0700

    Feat: vehicles.edit view created

---
+++ resources/views/vehicles/edit.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <h1>Edit Work Order: {{ $vehicle->work_order}}</h1>
+    
+    <div class="row justify-content-center">
+      <div class="col">
+        <form action="/vehicles/{{ $vehicle->id }}" method="post">
+
+          {{-- Authtentication Token --}}
+          @csrf
+          @method('PATCH')
+  
+          <div class="form-group row">
+            <label for="work_order" class="col-md-3 col-form-label text-md-right">Work Order</label>
+  
+            <div class="col-md-7">
+                <input id="work_order" type="text" class="form-control @error('work_order') is-invalid @enderror" name="work_order" value="{{ $vehicle->work_order }}" required autocomplete="work_order" autofocus>
+  
+                @error('work_order')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label for="client_id" class="col-md-3 col-form-label text-md-right">Client's Account</label>
+            <div class="col-md-4">
+              <select name="client_id" class="form-control" id="client_id">
+
+                {{-- It preselects the correct client coming to DB --}}
+                @foreach ($clients as $client)
+                  <option 
+                  value="{{ $client->id }}" 
+                  {{ $client->id == $vehicle->client->id ? 'selected' : '' }}
+                  >{{ $client->name }}</option>
+                @endforeach
+
+              </select>
+            </div>
+          </div>
+  
+          <div class="row">
+            <div class="col-md-8 offset-3">
+              <div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <a href="/" class="btn btn-secondary">Cancel</a>
+              </div>
+            </div>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
