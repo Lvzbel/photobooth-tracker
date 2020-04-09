bc1eb0765ed7122cabc55e15dd0f48bb8ffcc1fb
Lvzbel <rcoto@protonmail.com>
2020-04-09 01:47:05 -0700

    Removed: vehicle delete button and moved to a component

--- resources/views/vehicles/edit.blade.php
+++ resources/views/vehicles/edit.blade.php
@@ -47,9 +47,7 @@
             <div class="col-md-8 offset-3 d-flex justify-content-between">
 
               {{-- Activates Delete Modal --}}
-              <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter">
-                Delete
-              </button>
+              <x-vehicles.delete-button :vehicle="$vehicle"></x-vehicles.delete-button>
 
               <div>
                 <button type="submit" class="btn btn-primary">Submit</button>
@@ -62,29 +60,4 @@
       </div>
     </div>
   </div>
-
-  <!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Delete Confirmation</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        Are you sure you want to delete Work Order: {{ $vehicle->work_order}}
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <form action="/vehicles/{{ $vehicle->id }}" method="post">
-          @csrf
-          @method('DELETE')
-          <button type="submit" class="btn btn-danger">Delete</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
 @endsection
