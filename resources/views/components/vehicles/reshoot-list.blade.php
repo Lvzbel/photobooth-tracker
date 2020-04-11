c3eb74bac85d9295a019921d2180bde0fab09f42
Lvzbel <rcoto@protonmail.com>
2020-04-10 18:15:47 -0700

    Feat: reshoot component now takes all vehicles and marks the current viewing vehicle

--- resources/views/components/vehicles/reshoot-list.blade.php
+++ resources/views/components/vehicles/reshoot-list.blade.php
@@ -1,9 +1,9 @@
 <h3>Re-shoots</h3>
-
-@unless ($vehicles->isEmpty())
+{{-- It will only display if the count is more than 1, if is one it means that only the current vehicle is avaible --}}
+@unless ($vehicles->count() == 1)
   <table class="table pt-4">
     <thead>
-      <tr class="table-primary">
+      <tr>
         <th scope="col">#</th>
         <th scope="col">Work Order</th>
         <th scope="col">Photographer</th>
@@ -12,7 +12,7 @@
     </thead>
     <tbody>
       @foreach ($vehicles as $key=>$vehicle)
-        <tr>
+        <tr class="{{ $current == $vehicle->id ? 'table-primary' : ''}}">
           <th scope="row">{{ $key+1 }}</th>
           <td><a href="/vehicles/{{ $vehicle->id }}">{{ $vehicle->work_order}}</a></td>
           <td>{{ $vehicle->user->name }}</td>
