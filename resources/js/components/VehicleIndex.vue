a1cb9a7667cc0fbb0a2cd50f9a38c85168a9cead
Lvzbel <rcoto@protonmail.com>
2020-04-15 22:05:12 -0700

    Feat: included selection calendar and is using moment js as well

--- resources/js/components/VehicleIndex.vue
+++ resources/js/components/VehicleIndex.vue
@@ -1,38 +1,55 @@
 <template>
-  <div>
-    <h4>List of Vehicles</h4>
-    <ul class="list-group">
-      <VehicleItem
-        class="list-group-item"
-        v-for="vehicle in filteredVehicles"
-        v-bind:key="vehicle.id"
-        :vehicle="vehicle"
-      ></VehicleItem>
-    </ul>
-  </div>
+    <div>
+        <h4>List of Vehicles</h4>
+        <v-date-picker v-model="range" mode="range" is-inline />
+
+        <ul class="list-group">
+            <VehicleItem
+                class="list-group-item"
+                v-for="vehicle in filteredVehicles"
+                v-bind:key="vehicle.id"
+                :vehicle="vehicle"
+            ></VehicleItem>
+        </ul>
+    </div>
 </template>
 
 <script>
+import VCalendar from "v-calendar";
 import VehicleItem from "./VehicleItem";
 
 export default {
-  components: {
-    VehicleItem
-  },
-  data: function() {
-    return {
-      filteredVehicles: []
-    };
-  },
-  mounted: function() {
-    axios
-      .get(
-        "http://photobooth-tracker.test/vehicles/date?end=2020-04-14&start=2020-04-05"
-      )
-      .then(response => (this.filteredVehicles = response.data));
-  }
+    components: {
+        VehicleItem,
+        VCalendar
+    },
+    data: function() {
+        return {
+            filteredVehicles: [],
+            range: {
+                start: moment()
+                    .subtract(4, "days")
+                    .toDate(),
+                end: moment().toDate()
+            }
+        };
+    },
+    computed: {
+        formatedStartDate() {
+            return moment(this.range.start).format("YYYY-MM-DD");
+        },
+        formatedEndDate() {
+            return moment(this.range.end).format("YYYY-MM-DD");
+        }
+    },
+    mounted: function() {
+        axios
+            .get(
+                `http://photobooth-tracker.test/vehicles/date?end=${this.formatedEndDate}&start=${this.formatedStartDate}`
+            )
+            .then(response => (this.filteredVehicles = response.data));
+    }
 };
 </script>
 
-<style>
-</style>
+<style></style>
