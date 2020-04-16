56f83dd710fad8f1eaf2bf8e6b1cdd1c2f4eaaa5
Lvzbel <rcoto@protonmail.com>
2020-04-15 20:39:49 -0700

    Feat: VehicleItem component created but not yet finished

---
+++ resources/js/components/VehicleItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="d-flex justify-content-between">
+    <div v-text="vehicle.work_order"></div>
+    <div v-text="vehicleDate"></div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["vehicle"],
+
+  computed: {
+    vehicleDate() {
+      return this.vehicle.created_at;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
