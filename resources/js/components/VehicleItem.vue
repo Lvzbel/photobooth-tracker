610b942bbf8ebcd8acd4105971ecacb90529b4d8
Lvzbel <rcoto@protonmail.com>
2020-04-15 20:53:01 -0700

    Feat: VehicleItem is using moment.js to diplay the date

--- resources/js/components/VehicleItem.vue
+++ resources/js/components/VehicleItem.vue
@@ -1,21 +1,21 @@
 <template>
-  <li class="d-flex justify-content-between">
-    <div v-text="vehicle.work_order"></div>
-    <div v-text="vehicleDate"></div>
-  </li>
+    <li class="d-flex justify-content-between">
+        <div v-text="vehicle.work_order"></div>
+        <div v-text="vehicleDate"></div>
+    </li>
 </template>
 
 <script>
 export default {
-  props: ["vehicle"],
+    props: ["vehicle"],
 
-  computed: {
-    vehicleDate() {
-      return this.vehicle.created_at;
+    computed: {
+        vehicleDate() {
+            let date = moment(this.vehicle.created_at);
+            return date.format("MMM Do YYYY");
+        }
     }
-  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
