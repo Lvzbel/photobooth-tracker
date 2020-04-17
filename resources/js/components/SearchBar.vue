6b89d9a105a858e227fc9e83975a5a104fe7b77b
Lvzbel <rcoto@protonmail.com>
2020-04-16 22:51:51 -0700

    Feat: Results are showing and disapering correctly

--- resources/js/components/SearchBar.vue
+++ resources/js/components/SearchBar.vue
@@ -1,31 +1,60 @@
 <template>
-  <div class="input-group mb-3">
-    <div class="input-group-prepend">
-      <span class="input-group-text" id="basic-addon1">@</span>
+  <div>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">@</span>
+      </div>
+      <input
+        v-model="search"
+        @focus="toggleResults"
+        @blur="clearResults"
+        type="text"
+        class="form-control"
+        placeholder="Search Work Order"
+        aria-label="Search work order"
+      />
     </div>
-    <input
-      v-model="search"
-      type="text"
-      class="form-control"
-      placeholder="Search Work Order"
-      aria-label="Search work order"
-    />
+    <ul v-show="results">
+      <VehicleItem
+        class="list-group-item"
+        v-for="vehicle in results"
+        v-bind:key="vehicle.id"
+        :vehicle="vehicle"
+      ></VehicleItem>
+    </ul>
   </div>
 </template>
 
 <script>
+import VehicleItem from "./VehicleItem";
+
 export default {
+  components: {
+    VehicleItem
+  },
   data: function() {
     return {
-      search: ""
+      search: "",
+      results: null
     };
+  },
+  methods: {
+    toggleResults() {
+      console.log("Toggle Working");
+    },
+    clearResults() {
+      setTimeout(() => {
+        this.results = null;
+        this.search = "";
+      }, 300);
+    }
   },
   watch: {
     search() {
       if (this.search.length >= 4 && this.search.length <= 7) {
         axios
           .get(`/vehicles/search?search=${this.search}`)
-          .then(response => console.log(response.data));
+          .then(response => (this.results = response.data));
       }
     }
   }
