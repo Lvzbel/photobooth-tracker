1b2e30509a52e0c92f7c2a9f88b29fa75fedf94e
Lvzbel <rcoto@protonmail.com>
2020-04-17 19:22:12 -0700

    Styles: basic css for counter is done

--- resources/js/components/OrderCounter.vue
+++ resources/js/components/OrderCounter.vue
@@ -1,12 +1,9 @@
 <template>
-  <div>
+  <div class="counter-main">
     <div class="container-counter">
-      <div class="counter" v-text="count"></div>
-    </div>
-    <div class="container-client">
-      <ul class="client-list">
-        <li class="client-item">Vroom</li>
-      </ul>
+      <div class="counter badge-primary">
+        <span id="counter-display" v-text="count">Primary</span>
+      </div>
     </div>
   </div>
 </template>
@@ -37,4 +34,24 @@ export default {
 </script>
 
 <style>
+.counter-main {
+  padding: 2rem;
+}
+.container-counter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.counter {
+  border-radius: 100%;
+  width: 120px;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#counter-display {
+  font-variant-numeric: tabular-nums;
+  font-size: 3rem;
+}
 </style>
