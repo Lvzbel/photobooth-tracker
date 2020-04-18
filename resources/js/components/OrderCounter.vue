215df8425be06413e952cb140c055825f133bf03
Lvzbel <rcoto@protonmail.com>
2020-04-17 18:13:30 -0700

    Feat: OrderCounter will display information about todays orders

---
+++ resources/js/components/OrderCounter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="container-counter">
+      <div class="counter" v-text="count"></div>
+    </div>
+    <div class="container-client">
+      <ul class="client-list">
+        <li class="client-item">Vroom</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      orders: []
+    };
+  },
+  methods: {
+    getOrders() {
+      axios
+        .get("/vehicles/today")
+        .then(response => (this.orders = response.data));
+    }
+  },
+  mounted: function() {
+    this.getOrders();
+  },
+  computed: {
+    count() {
+      return this.orders.length;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
