76b3b8d8d3dca668675898a034a447cb46a1310c
Lvzbel <rcoto@protonmail.com>
2020-04-17 17:08:18 -0700

    Feat: basic layout for admin planel created

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app');
 
 @section('content')
-    hevo
+    <div class="container">
+      <h1 class="mb-4">Admin Panel</h1>
+      <div class="row my-4 d-flex justify-content-center">
+        <div class="col col-lg-6">
+          <h3>Number of Orders Today</h3>
+        </div>
+        <div class="col col-lg-6">
+          <h3>Current Clients</h3>
+        </div>
+      </div>
+      <div class="row my-4 d-flex justify-content-center">
+        <div class="col">
+          <h3>Roles List</h3>
+        </div>
+      </div>
+    </div>
 @endsection
