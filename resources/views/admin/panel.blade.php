08986fbbb852157ae519533c4aa0951bfdbc4170
Lvzbel <rcoto@protonmail.com>
2020-04-17 18:13:57 -0700

    Feat: included OrderCounter in vue

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -6,6 +6,7 @@
       <div class="row my-4 d-flex justify-content-center">
         <div class="col col-lg-6">
           <h3>Number of Orders Today</h3>
+          <ordercounter></ordercounter>
         </div>
         <div class="col col-lg-6">
           <h3>Current Clients</h3>
