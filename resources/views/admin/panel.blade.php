7a572dfc68419a552be65b06a8dee016e867660c
Lvzbel <rcoto@protonmail.com>
2020-04-20 22:13:41 -0700

    Refact: commented labeled vue componets and added UserList

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -3,10 +3,13 @@
 @section('content')
     <div class="container">
       <h1 class="mb-4">Admin Panel</h1>
+      {{-- Vue Component AdminPanel --}}
         <admintoppanel></admintoppanel>
       <div class="row my-4 d-flex justify-content-center">
         <div class="col">
           <h3>Roles List</h3>
+          {{-- Vue Componet UserList --}}
+          <userlist></userlist>
         </div>
       </div>
     </div>
