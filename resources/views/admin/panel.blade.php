e428576ef0e6b37da43cf3e46cde45a26c82cd77
Lvzbel <rcoto@protonmail.com>
2020-04-21 00:58:50 -0700

    Feat: userlist is only showed to admins and managers

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -5,15 +5,14 @@
       <h1 class="mb-4">Admin Panel</h1>
       {{-- Vue Component AdminPanel --}}
         <admintoppanel></admintoppanel>
+        @can('viewany', $user)
       <div class="row my-4 d-flex justify-content-center">
         <div class="col">
           <h3>Roles List</h3>
           {{-- Vue Componet UserList --}}
-          @can('viewany', $user)
           <userlist></userlist>
-          @endcan
-          
         </div>
       </div>
+      @endcan
     </div>
 @endsection
