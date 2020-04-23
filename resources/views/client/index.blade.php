7603e4682c5a5c25b2f3c9369d3a66191aa33864
Lvzbel <rcoto@protonmail.com>
2020-04-22 22:27:42 -0700

    Created edit link for index and a dummy delete button

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -6,8 +6,12 @@
     <div class="col col-md-8">
       <ul class="list-group">
         @foreach ($clients as $client)
-          <li class="list-group-item">
+          <li class="list-group-item d-flex justify-content-between align-items-center">
             <div>{{$client->name}}</div>
+            <div>
+              <a class="btn btn-secondary" href="/clients/{{$client->id}}/edit">Edit</a>
+              <button class="btn btn-danger">Delete</button>
+            </div>
           </li>
         @endforeach
       </ul>
