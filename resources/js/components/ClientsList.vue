1d7af3ac250b0f10aed08632d5915330b752121d
Lvzbel <rcoto@protonmail.com>
2020-05-04 00:33:59 -0700

    Feat: ClientList is showing logos

--- resources/js/components/ClientsList.vue
+++ resources/js/components/ClientsList.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="client-main">
     <ul class="client-list list-group">
-      <li
-        v-for="client in clients"
-        class="client-item list-group-item"
-        v-text="client.name"
-        :key="client.id"
-      ></li>
+      <li v-for="client in clients" class="client-item list-group-item" :key="client.id">
+        <img class="client__logo client__logo--small" :src="client.image" alt />
+        {{ client.name }}
+      </li>
     </ul>
   </div>
 </template>
