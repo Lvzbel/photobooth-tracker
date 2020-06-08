c3d580530f2f4d8d58766d0aaf9852bd1de4f109
Lvzbel <rcoto@protonmail.com>
2020-06-07 23:07:02 -0700

    Refact: V-Calendar removed

--- resources/js/components/VehicleIndex.vue
+++ resources/js/components/VehicleIndex.vue
@@ -2,13 +2,6 @@
     <div>
         <h4>List of Vehicles</h4>
         <div class="row d-flex justify-content-center my-5">
-            <v-date-picker
-                v-model="range"
-                mode="range"
-                is-inline
-                :columns="$screens({ default: 1, md: 2 })"
-                :availableDates="{ start: minDate, end: maxDate }"
-            />
             <AirbnbCalendar />
         </div>
 
