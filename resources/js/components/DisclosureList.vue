faa3174cca0dba864bc3603078ef36c2d0641b04
Lvzbel <rcoto@protonmail.com>
2020-10-04 00:50:35 -0700

    Feat: single disclosure working

---
+++ resources/js/components/DisclosureList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="item in list" :key="item">
+      {{ item }} <button @click="onSelect(item)">Select</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    list: { type: Array, required: true },
+  },
+  methods: {
+    onSelect(selection) {
+      this.$emit("on-select", "hevo");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
