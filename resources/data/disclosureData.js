00aeab61b65737578bd3342918e2631e30430410
Lvzbel <rcoto@protonmail.com>
2020-10-03 23:30:05 -0700

    Feat: added disclosure data

---
+++ resources/data/disclosureData.js
@@ -0,0 +1,44 @@
+export const areas = [
+    "front",
+    "driver side",
+    "rear",
+    "passenger side",
+    "roof",
+    "interior"
+];
+
+export const damageType = ["chip", "scratch", "scuff", "dent", "other"];
+
+export const locations = {
+    front: ["hood", "front bumper", "headlights"],
+    driver: [
+        "fender",
+        "front wheel",
+        "mirror",
+        "front door",
+        "rear door",
+        "quarter panel"
+    ],
+    rear: ["deck lid", "rear bumper", "taillights"],
+    passenger: [
+        "fender",
+        "front wheel",
+        "mirror",
+        "front door",
+        "rear door",
+        "quarter panel"
+    ],
+    roof: ["windshield", "roof", "windows"],
+    interior: [
+        "driver seat",
+        "front passenger seat",
+        "rear passenger seats",
+        "third row seats",
+        "dash",
+        "steering wheel",
+        "headliner",
+        "carpet",
+        "center console",
+        "misc trim"
+    ]
+};
