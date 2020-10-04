9c16a430382aa8aa0da8d9193ed5c4adc69d53a8
Lvzbel <rcoto@protonmail.com>
2020-10-04 01:07:05 -0700

    Feat: location selection showing properly according to area

--- resources/js/components/DisclosureForm.vue
+++ resources/js/components/DisclosureForm.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
     <h2>Please add anywhere from 4 to 6 disclosures</h2>
+    <!-- Area -->
     <div>
       <div v-if="!selectedArea">Select an Area</div>
       <div v-if="selectedArea">Selected Area: {{ selectedArea }}</div>
+      <DisclosureList v-if="!selectedArea" :list="areas" @on-select="setArea" />
+    </div>
+    <!-- Location -->
+    <div v-if="selectedArea">
+      <div v-if="!selectedLocation">Select Location</div>
+      <div v-if="selectedLocation">
+        Selected Location: {{ selectedLocation }}
+      </div>
       <DisclosureList
-        v-if="!selectedArea"
-        :list="areas"
+        v-if="!selectedLocation"
+        :list="locations[computedLocation]"
         @on-select="setLocation"
       />
     </div>
-    <div v-if="selectedArea">Locations</div>
     <div v-if="selectedLocation">Damage Type</div>
   </div>
 </template>
@@ -44,6 +52,25 @@ export default {
     },
     onSubmit() {
       console.log("submit");
+    },
+  },
+  computed: {
+    computedLocation() {
+      let location;
+      switch (this.selectedArea) {
+        case "front":
+          return "front";
+        case "driver side":
+          return "driver";
+        case "rear":
+          return "rear";
+        case "passenger side":
+          return "passenger";
+        case "roof":
+          return "roof";
+        case "interior":
+          return "interior";
+      }
     },
   },
 };
