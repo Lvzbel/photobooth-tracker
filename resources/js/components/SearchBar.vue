8e771c2bfe4fcf4bfae8fd23e3d567e16313ec4d
Lvzbel <rcoto@protonmail.com>
2020-10-04 23:34:55 -0700

    Feat: Searchbar now display a not found message

--- resources/js/components/SearchBar.vue
+++ resources/js/components/SearchBar.vue
@@ -1,75 +1,81 @@
 <template>
-    <div>
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon1"
-                    ><i class="fas fa-search text-primary"></i
-                ></span>
-            </div>
-            <input
-                v-model="search"
-                @focus="toggleResults"
-                @blur="clearResults"
-                type="text"
-                class="form-control"
-                placeholder="Search Work Order"
-                aria-label="Search work order"
-            />
-        </div>
-        <ul class="results p-0" v-show="results">
-            <VehicleItem
-                class="list-group-item"
-                v-for="vehicle in results"
-                v-bind:key="vehicle.id"
-                :vehicle="vehicle"
-            ></VehicleItem>
-        </ul>
+  <div>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1"
+          ><i class="fas fa-search text-primary"></i
+        ></span>
+      </div>
+      <input
+        v-model="search"
+        @focus="toggleResults"
+        @blur="clearResults"
+        type="text"
+        class="form-control"
+        placeholder="Search Work Order"
+        aria-label="Search work order"
+      />
     </div>
+    <ul class="results p-0" v-show="results">
+      <VehicleItem
+        class="list-group-item"
+        v-for="vehicle in results"
+        v-bind:key="vehicle.id"
+        :vehicle="vehicle"
+      ></VehicleItem>
+      <li v-if="notFound" class="list-group-item">Order Not Found!</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import VehicleItem from "./VehicleItem";
 
 export default {
-    components: {
-        VehicleItem
+  components: {
+    VehicleItem,
+  },
+  data: function () {
+    return {
+      search: "",
+      results: null,
+    };
+  },
+  methods: {
+    toggleResults() {
+      console.log("Toggle Working");
     },
-    data: function() {
-        return {
-            search: "",
-            results: null
-        };
+    clearResults() {
+      setTimeout(() => {
+        this.results = null;
+        this.search = "";
+      }, 300);
     },
-    methods: {
-        toggleResults() {
-            console.log("Toggle Working");
-        },
-        clearResults() {
-            setTimeout(() => {
-                this.results = null;
-                this.search = "";
-            }, 300);
-        }
+  },
+  watch: {
+    search() {
+      if (this.search.length >= 4 && this.search.length <= 7) {
+        axios
+          .get(`/vehicles/search?search=${this.search}`)
+          .then((response) => (this.results = response.data));
+      }
     },
-    watch: {
-        search() {
-            if (this.search.length >= 4 && this.search.length <= 7) {
-                axios
-                    .get(`/vehicles/search?search=${this.search}`)
-                    .then(response => (this.results = response.data));
-            }
-        }
-    }
+  },
+  computed: {
+    notFound() {
+      return Array.isArray(this.results) && this.results.length === 0;
+    },
+  },
 };
 </script>
 
 <style scope>
 .input-group {
-    position: relative;
+  position: relative;
 }
 .results {
-    position: absolute;
-    width: 95%;
-    z-index: 50;
+  position: absolute;
+  width: 95%;
+  z-index: 50;
 }
 </style>
